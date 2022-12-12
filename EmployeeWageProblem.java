3a55c33e5ccb2cbc1eaaab5a7e3eaae44df960bd
gaurav459433 <gaurav459433@gmail.com>
2022-12-13 00:26:46 +0530

    Employee Present Absent Logic

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.day03.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
+        Random random = new Random();
+        int a = random.nextInt(2);
+        if (a == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
