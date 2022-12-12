9e517c1b363e0fd77bd17842d920406452b27573
gaurav459433 <gaurav459433@gmail.com>
2022-12-13 01:08:12 +0530

    Employee PartTime Wage Logic

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -6,13 +6,16 @@ public class EmployeeWageProblem {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
         Random random = new Random();
-        int a = random.nextInt(2);
+        int a = random.nextInt(3);
         int wagePH = 20;
         int fullDayH = 8;
+        int partTimeDayH = 4;
         if (a == 1) {
-            System.out.println("Employee is Present\nPer Day Wage is :"+ " " +(wagePH * fullDayH));
+            System.out.println("Employee is Present\nPer Day Wage is :" + " " + (wagePH * fullDayH));
+        } else if (a == 2) {
+            System.out.println("Employee is Present Halfday\nPer Day Wage is :" + " " + (wagePH * partTimeDayH));
         } else {
-            System.out.println("Employee is Absent\nPer Day Wage is :"+ " " +(0));
+            System.out.println("Employee is Absent\nPer Day Wage is :" + " " + (0));
         }
     }
 }
