bccc7e236ef5dd982605f740cbd746cf9c424136
gaurav459433 <gaurav459433@gmail.com>
2022-12-13 00:53:39 +0530

    Employee Daily Wage Logic

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -7,10 +7,12 @@ public class EmployeeWageProblem {
         System.out.println("Welcome To Employee Wage Computation Program");
         Random random = new Random();
         int a = random.nextInt(2);
+        int wagePH = 20;
+        int fullDayH = 8;
         if (a == 1) {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present\nPer Day Wage is :"+ " " +(wagePH * fullDayH));
         } else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent\nPer Day Wage is :"+ " " +(0));
         }
     }
 }
